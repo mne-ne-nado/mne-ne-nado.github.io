bcf6fee20004ddf62fa861d95b561c46c2296de6
mne-ne-nado <svetlana.kamolikova@gmail.com>
2017-03-19 19:56:57 +0300

    bugs

--- assets/js/material-kit.js
+++ assets/js/material-kit.js
@@ -71,24 +71,6 @@ materialKit = {
     }, 17),
 
     initSliders: function(){
-        // Sliders for demo purpose
-        $('#sliderRegular').noUiSlider({
-            start: 40,
-            connect: "lower",
-            range: {
-                min: 0,
-                max: 100
-            }
-        });
-
-        $('#sliderDouble').noUiSlider({
-            start: [20, 60] ,
-            connect: true,
-            range: {
-                min: 0,
-                max: 100
-            }
-        });
     }
 }
 
